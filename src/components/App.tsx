87f63f0d09e37dd92ca91767dd1e99ac9de8799e
kimsh0117 <crea.id.1703@gmail.com>
2019-03-18 00:03:55 +0300

    Add helmel and Change favicon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { routes } from 'lib/routes';
+import { Helmet } from 'react-helmet';
 
 const App: React.FC = () => (
   <>
-    {routes.map(({ path, page, exact }, i) => (
-      <Route exact={exact} path={path} component={page} key={i} />
-    ))}
+    <Helmet>
+      <title>ShopMate</title>
+      <meta
+        name="description"
+        content="Turing front-end e-commerce system code"
+      />
+      <meta property="fb:app_id" content="203040656938507" />
+      <meta property="og:image" content="https://images.velog.io/velog.png" />
+    </Helmet>
+    <Switch>
+      {routes.map(({ path, page, exact }, i) => (
+        <Route exact={exact} path={path} component={page} key={i} />
+      ))}
+    </Switch>
   </>
 )
 
